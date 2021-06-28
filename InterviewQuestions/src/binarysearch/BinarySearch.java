1881e3db505324b495acd1bf71239d8734b01433
Yiyi Zhang <yiyiz@amazon.com>
2021-06-27 17:39:53 -0700

    binarysearch comments

--- InterviewQuestions/src/binarysearch/BinarySearch.java
+++ InterviewQuestions/src/binarysearch/BinarySearch.java
@@ -1,5 +1,9 @@
 package binarysearch;
 
 public class BinarySearch {
-
+	// https://www.geeksforgeeks.org/variants-of-binary-search/
+	// while 永远是 while(left <= right)
+	// 精确搜索，是否contain：mid = low + (high - low) / 2;
+	// 模糊搜素：第一次或最后一次occurrence/最接近某个值 mid = low + (high - low + 1) / 2;
+	// 永远是 low = middle + 1 和 high = middle - 1
 }
